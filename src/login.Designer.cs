3c93411ce78da79236414cc8c3ecf0f3efac2570
rperal22 <rperal@gmail.com>
2017-05-24 21:42:14 -0300

    actualizaciones del proyecto

--- src/login.Designer.cs
+++ src/login.Designer.cs
@@ -90,6 +90,7 @@
             this.button1.TabIndex = 5;
             this.button1.Text = "Limpiar";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
--- src/login.cs
+++ src/login.cs
@@ -86,4 +86,14 @@ namespace UberFrba
             }           
         }
     }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+               
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
 }}
